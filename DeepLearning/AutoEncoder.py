586a7e977c144da42ea287fabed4da1f40924335
wwbweibo <wwbweibo@outlook.com>
2018-11-19 17:58:29 +0800

    修改部分文件

--- DeepLearning/AutoEncoder.py
+++ DeepLearning/AutoEncoder.py
@@ -11,91 +11,135 @@ from keras.callbacks import TensorBoard
 from keras.models import Model
 from matplotlib import pyplot as plt
 from gaps_dataset import gaps
-from DataLoader import load_gaps_crack_images
+from DataLoader import load_gaps_crack_images, load_gaps
+import time
+import os
 
+class AutoEncoder:
+    def __init__(self, weights_path):
+        """
+        this is a AutoEncoder that will use as the first layers of the model
+        we merge the encoder and the decoder hidden layers' input as the bbox regression input 
+        and use the encoder output as the classifer regression input
+        @param weigth_path: the pre-trained model weights path.
+        """
+        self.encoder = self.__gen_encoder__()
+        self.decoder = self.__gen_decoder__()
+        self.auto_encoder = self.__gen_autoencoder__()
+        if weights_path is None:
+            raise("please input the weights path")
+        if os.path.exists(weights_path): 
+            self.encoder.load_weights(weights_path+"/encoder.h5")
+            self.decoder.load_weights(weights_path+"/decoder.h5")
+            self.encoder.load_weights(weights_path+"/autoencoder.h5")
 
-def encoder_conv_block(ipt, filter, stage, conv_layer=2, kernel_size=(3, 3)):
-    x = ipt
-    layer_name = "encoder_stage%d_%s_%d"
-    for i in range(conv_layer):
-        x = Conv2D(filter, kernel_size, padding='same',
-                   name=layer_name % (stage, 'conv', i))(x)
-        x = BatchNormalization(momentum=0.8)(x)
-        x = Activation('relu')(x)
-    return x
+    def __encoder_conv_block__(self, ipt, filter, stage, conv_layer=2, kernel_size=(3, 3)):
+        x = ipt
+        layer_name = "encoder_stage%d_%s_%d"
+        for i in range(conv_layer):
+            x = Conv2D(filter, kernel_size, padding='same',
+                       name=layer_name % (stage, 'conv', i))(x)
+            x = BatchNormalization(momentum=0.8)(x)
+            x = Activation('relu')(x)
+        return x
 
+    def __decoder_conv_block__(self, ipt, filter, stage):
+        x = ipt
+        layer_name = "decoder_stage%d_%s_%d"
+        x = Conv2D(filter, (3, 3), activation='relu', padding='same',
+                   name=layer_name % (stage, 'conv', 1))(x)
+        x = Conv2D(filter, (3, 3), activation='relu', padding='same',
+                   name=layer_name % (stage, 'conv', 2))(x)
+        return x
 
-def decoder_conv_block(ipt, filter, stage):
-    x = ipt
-    layer_name = "decoder_stage%d_%s_%d"
-    x = Conv2D(filter, (3, 3), activation='relu', padding='same', name=layer_name%(stage, 'conv', 1))(x)
-    x = Conv2D(filter, (3, 3), activation='relu', padding='same', name=layer_name%(stage, 'conv', 2))(x)
-    return x
+    def __gen_encoder__(self):
+        encoder_input = Input(shape=(None, None, 1), name='encoder_input')
+        stage1 = self.__encoder_conv_block__(encoder_input, 16, 1)
+        stage1 = MaxPool2D((2, 2))(stage1)
+        # 32
+        stage2 = self.__encoder_conv_block__(stage1, 32, 2, conv_layer=3)
+        stage2 = MaxPool2D((2, 2))(stage2)
+        # 16
+        stage3 = self.__encoder_conv_block__(stage2, 64, 3, conv_layer=3)
+        stage3 = MaxPool2D((2, 2))(stage3)
+        # 8
+        stage4 = self.__encoder_conv_block__(stage3, 128, 4, conv_layer=4)
+        stage4 = MaxPool2D((2, 2))(stage4)
+        # 4
+        stage5 = self.__encoder_conv_block__(stage4, 256, 5, conv_layer=4)
 
-def gen_encoder():
-    encoder_input = Input(shape=(None, None, 1), name='encoder_input')
-    stage1 = encoder_conv_block(encoder_input, 32, 1)
-    stage1 = MaxPool2D((2, 2))(stage1)
+        output = Conv2D(16, (1, 1), padding='same',
+                        activation='relu', name='encoder_outout')(stage5)
 
-    stage2 = encoder_conv_block(stage1, 32, 2, conv_layer=3)
-    stage2 = MaxPool2D((2, 2))(stage2)
+        encoder = Model(encoder_input, output)
+        encoder.summary()
+        return encoder
 
-    stage3 = encoder_conv_block(stage2, 64, 3, conv_layer=3)
-    stage3 = MaxPool2D((2, 2))(stage3)
+    def __gen_decoder__(self):
+        decoder_input = Input(shape=(None, None, 16), name='decoder_input')
 
-    stage4 = encoder_conv_block(stage3, 128, 4, conv_layer=4)
+        stage0 = self.__decoder_conv_block__(decoder_input, 256, 0)
+        stage0 = UpSampling2D((2, 2))(stage0)
 
-    output = Conv2D(32, (1, 1), padding='same', activation='relu', name='encoder_outout')(stage4)
+        stage1 = self.__decoder_conv_block__(stage0, 128, 1)
+        stage1 = UpSampling2D((2, 2))(stage1)
+        # 16
+        stage2 = self.__decoder_conv_block____(stage1, 64, 2)
+        stage2 = UpSampling2D((2, 2))(stage2)
+        # 32
+        stage3 = self.__decoder_conv_block__(stage2, 32, 3)
+        stage3 = UpSampling2D((2, 2))(stage3)
 
-    encoder = Model(encoder_input, output)
-    encoder.summary()
-    return encoder
+        stage4 = self.__decoder_conv_block__(stage3, 16, 4)
+        output = Conv2D(1, (1, 1), activation='sigmoid',
+                        padding='same', name='decoder_output')(stage4)
 
+        decoder = Model(decoder_input, output)
+        decoder.summary()
+        return decoder
 
-def gen_decoder():
-    decoder_input = Input(shape=(None, None, 32), name='decoder_input')
+    def __gen_autoencoder__(self):
+        autoencoder_input = Input(
+            shape=(None, None, 1), name='autoencoder_input')
+        x = self.encoder(autoencoder_input)
+        output = self.decoder(x)
 
-    stage1 = decoder_conv_block(decoder_input, 32, 1)
-    stage1 = UpSampling2D((2, 2))(stage1)
+        autoencoder = Model(autoencoder_input, output)
+        autoencoder.compile(optimizer='adam', loss='mse')
+        return autoencoder
 
-    stage2 = decoder_conv_block(stage1, 32, 2)
-    stage2 = UpSampling2D((2, 2))(stage2)
+    def train(self, epochs=100, gaps_datasets=[0,1], log_dir='./logs'):
+        """
+        this method will train the AutoEncoder.
+        as default run 100 epochs in gaps dataset chunk 0 and 1. the training history will save in dir ```./logs```
+        to see the history please using ```tensorboard```
+        """
+        data = load_gaps_crack_images(gaps_datasets)
+        self.auto_encoder.fit(x=data[0:7000], y=data[0:7000],
+                              batch_size=16, epochs=epochs,
+                              callbacks=[TensorBoard(log_dir='./logs')],
+                              validation_data=[data[7001:], data[7001:]])
 
-    stage3 = decoder_conv_block(stage2, 16, 3)
-    stage3 = UpSampling2D((2, 2))(stage3)
+    def test_model(self, model, weights_name):
+        model.load_weights(weights_name)
+        x, _ = load_gaps()
+        predict = model.predict(np.asarray([x[10]]))
+        plt.subplot(1, 2, 1)
+        plt.imshow(x[0, :, :, 0], cmap='gray')
+        plt.subplot(1, 2, 2)
+        plt.imshow(np.reshape(predict[0], (64, 64)), cmap='gray')
+        plt.show()
+        im = cv2.imread(
+            '/media/weibo/Data/Code/RoadDetect/test_img/timg.jpg', cv2.IMREAD_GRAYSCALE)
+        # im = cv2.imread('E:/Code/RoadDetect/test_img/th1.jpg', cv2.IMREAD_GRAYSCALE)
+        im = im.astype(np.float)
+        im /= 255
+        ret = model.predict([im.reshape((1, im.shape[0], im.shape[1], 1))])
+        plt.imshow(ret[0, :, :, 0], cmap='gray')
+        plt.show()
 
-    stage4 = decoder_conv_block(stage3, 16, 4)
-    output = Conv2D(1, (1, 1), activation='sigmoid', padding='same', name='decoder_output')(stage4)
-
-    decoder = Model(decoder_input, output)
-    decoder.summary()
-    return decoder
-
-
-def gen_autoencoder(encoder, decoder):
-    autoencoder_input = Input(shape=(None, None, 1), name='autoencoder_input')
-    x = encoder(autoencoder_input)
-    output = decoder(x)
-
-    autoencoder = Model(autoencoder_input, output)
-    autoencoder.compile(optimizer='adam', loss='mse')
-    return autoencoder
-
-
-def train(model, epochs=100):
-    data = load_gaps_crack_images([0])
-    model.fit(x=data, y=data, batch_size=16, epochs=epochs, callbacks=[TensorBoard(log_dir='./logs')])
-
-
-def main():
-    encoder = gen_encoder()
-    decoder = gen_decoder()
-    autoencoder = gen_autoencoder(encoder, decoder)
-    train(autoencoder)
-    encoder.save_weights('models/encoder.h5')
-    decoder.save_weights('models/decoder.h5')
-    autoencoder.save_weights('models/autoencoder.h5')
-
-
-if __name__ == '__main__':
-    main()
+    def get_models(self):
+        """
+        return the encoder, decoder and autoencoder
+        """
+        return  self.encoder, self.decoder, self.auto_encoder
--- DeepLearning/DataLoader.py
+++ DeepLearning/DataLoader.py
@@ -1,9 +1,13 @@
+"""
+contains some function that using to load datasets
+"""
 from gaps_dataset import gaps
 import numpy as np
 import keras.utils
 from stl10_input import read_all_images, read_labels
 
 def load_gaps():
+
     x, y = gaps.load_chunk(0, datadir='./', debug_outputs=True)
     x = np.reshape(x, (x.shape[0], 64, 64, 1))
     for im, idx in zip(x, range(32000)):
@@ -13,6 +17,10 @@ def load_gaps():
 
 
 def load_data():
+    """
+    a functio to load stl_10 binary dataset
+    the dataset save in ```stl10_binary``` folder
+    """
     x = read_all_images('stl10_binary/train_X.bin')
     y = read_labels('stl10_binary/train_y.bin')
     y = y - 1
@@ -27,18 +35,22 @@ def load_data():
 
 
 def load_gaps_crack_images(chunk_list):
-    ret_lst = []
+    """
+    from the gaps dataset load the crack images
+    @param chunk_list: a list that include the chunks which need to load
+    """
+    ret = None
     for chunk in chunk_list:
         x, y = gaps.load_chunk(chunk, datadir='.', debug_outputs=True)
         # find the 1 label
-        ret = np.where(y == 1)
         crack_x = x[np.where(y==1)]
         crack_x = np.reshape(crack_x, (crack_x.shape[0], 64, 64, 1))
         for im, idx in zip(crack_x, range(crack_x.shape[0])):
             if np.min(im) < 0:
                 crack_x[idx] = im + np.abs(np.min(im))
-        ret_lst.append(crack_x)
-    ret = np.asarray(ret_lst)
-    ret = np.reshape(ret, (-1, 64, 64, 1))
+        if type(ret) == type(None):
+            ret = crack_x
+        else:
+            ret = np.vstack((ret, crack_x))
     return ret
     
