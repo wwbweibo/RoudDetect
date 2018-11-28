4ead2dfa0c9021e596714fd26e0b1f8678d05a77
wwbweibo <wwbweibo@outlook.com>
2018-11-28 16:51:06 +0800

    ready  for training pipeline

--- DeepLearning/AutoEncoder.py
+++ DeepLearning/AutoEncoder.py
@@ -2,8 +2,8 @@ import cv2
 import keras.backend as K
 import keras.utils
 import numpy as np
-from keras.layers import Conv2D, Dense, Dropout, Flatten, Input, MaxPool2D, UpSampling2D
-from keras.layers import BatchNormalization, Activation
+from keras.layers import Conv2D, Dense, Dropout, Flatten, Input, MaxPool2D, UpSampling2D, AveragePooling2D
+from keras.layers import BatchNormalization, Activation, Lambda
 from keras.layers.merge import Add
 from keras.layers.advanced_activations import ThresholdedReLU
 from keras.losses import categorical_crossentropy
@@ -11,36 +11,45 @@ from keras.callbacks import TensorBoard
 from keras.models import Model
 from matplotlib import pyplot as plt
 from gaps_dataset import gaps
-from DataLoader import load_gaps_crack_images, load_gaps
+from DataLoader import load_gaps_crack_images, load_gaps, load_gaps_cut
 import time
 import os
 
+
 class AutoEncoder:
-    def __init__(self, weights_path):
+    def __init__(self, pre_trained=False, weights_path=None):
         """
         this is a AutoEncoder that will use as the first layers of the model
         we merge the encoder and the decoder hidden layers' input as the bbox regression input 
         and use the encoder output as the classifer regression input
-        @param weigth_path: the pre-trained model weights path.
+        @param pre_trained: is this model pre-trained
+        @param weigth_path: the pre-trained model weights path. if the ```pre_trained``` is true, must have a value
         """
         self.encoder = self.__gen_encoder__()
         self.decoder = self.__gen_decoder__()
         self.auto_encoder = self.__gen_autoencoder__()
-        if weights_path is None:
-            raise("please input the weights path")
-        if os.path.exists(weights_path): 
-            self.encoder.load_weights(weights_path+"/encoder.h5")
-            self.decoder.load_weights(weights_path+"/decoder.h5")
-            self.encoder.load_weights(weights_path+"/autoencoder.h5")
+        if pre_trained:
+            if weights_path is None:
+                raise ValueError(
+                    "weights_path must has a value if pre-trained is true")
+            if os.path.exists(weights_path+"_encoder.h5"):
+                self.encoder.load_weights(weights_path+"_encoder.h5")
+                self.decoder.load_weights(weights_path+"_decoder.h5")
+                self.auto_encoder.load_weights(weights_path+"_autoencoder.h5")
+            else:
+                raise ValueError("path is not exist")
 
     def __encoder_conv_block__(self, ipt, filter, stage, conv_layer=2, kernel_size=(3, 3)):
         x = ipt
         layer_name = "encoder_stage%d_%s_%d"
-        for i in range(conv_layer):
-            x = Conv2D(filter, kernel_size, padding='same',
+        _x = Conv2D(filter, kernel_size, padding='same',
+                    name=layer_name % (stage, 'conv', 0))(x)
+        for i in range(1, conv_layer+1):
+            x = Conv2D(filter, kernel_size, padding='same', kernel_initializer="normal",
                        name=layer_name % (stage, 'conv', i))(x)
-            x = BatchNormalization(momentum=0.8)(x)
-            x = Activation('relu')(x)
+            x = BatchNormalization()(x)
+        x = Add()([x, _x])
+        x = Activation('relu', name=layer_name % (stage, 'activation', 1))(x)
         return x
 
     def __decoder_conv_block__(self, ipt, filter, stage):
@@ -55,39 +64,34 @@ class AutoEncoder:
     def __gen_encoder__(self):
         encoder_input = Input(shape=(None, None, 1), name='encoder_input')
         stage1 = self.__encoder_conv_block__(encoder_input, 16, 1)
-        stage1 = MaxPool2D((2, 2))(stage1)
+        stage1 = AveragePooling2D((2, 2))(stage1)
         # 32
-        stage2 = self.__encoder_conv_block__(stage1, 32, 2, conv_layer=3)
-        stage2 = MaxPool2D((2, 2))(stage2)
+        stage2 = self.__encoder_conv_block__(stage1, 16, 2, conv_layer=3)
+        stage2 = AveragePooling2D((2, 2))(stage2)
         # 16
-        stage3 = self.__encoder_conv_block__(stage2, 64, 3, conv_layer=3)
-        stage3 = MaxPool2D((2, 2))(stage3)
+        stage3 = self.__encoder_conv_block__(stage2, 32, 3, conv_layer=3)
+        stage3 = AveragePooling2D((2, 2))(stage3)
         # 8
-        stage4 = self.__encoder_conv_block__(stage3, 128, 4, conv_layer=4)
-        stage4 = MaxPool2D((2, 2))(stage4)
-        # 4
-        stage5 = self.__encoder_conv_block__(stage4, 256, 5, conv_layer=4)
+        stage4 = self.__encoder_conv_block__(stage3, 32, 4, conv_layer=4)
 
-        output = Conv2D(16, (1, 1), padding='same',
-                        activation='relu', name='encoder_outout')(stage5)
+        # output = Conv2D(16, (1, 1), padding='same',
+        #                 activation='relu', name='encoder_outout')(stage5)
 
+        output = Conv2D(4, (1, 1), padding='same',
+                        activation='relu', name='encoder_outout')(stage4)
         encoder = Model(encoder_input, output)
-        encoder.summary()
         return encoder
 
     def __gen_decoder__(self):
-        decoder_input = Input(shape=(None, None, 16), name='decoder_input')
+        decoder_input = Input(shape=(None, None, 4), name='decoder_input')
 
-        stage0 = self.__decoder_conv_block__(decoder_input, 256, 0)
-        stage0 = UpSampling2D((2, 2))(stage0)
-
-        stage1 = self.__decoder_conv_block__(stage0, 128, 1)
+        stage1 = self.__decoder_conv_block__(decoder_input, 32, 1)
         stage1 = UpSampling2D((2, 2))(stage1)
         # 16
-        stage2 = self.__decoder_conv_block____(stage1, 64, 2)
+        stage2 = self.__decoder_conv_block__(stage1, 32, 2)
         stage2 = UpSampling2D((2, 2))(stage2)
         # 32
-        stage3 = self.__decoder_conv_block__(stage2, 32, 3)
+        stage3 = self.__decoder_conv_block__(stage2, 16, 3)
         stage3 = UpSampling2D((2, 2))(stage3)
 
         stage4 = self.__decoder_conv_block__(stage3, 16, 4)
@@ -95,7 +99,6 @@ class AutoEncoder:
                         padding='same', name='decoder_output')(stage4)
 
         decoder = Model(decoder_input, output)
-        decoder.summary()
         return decoder
 
     def __gen_autoencoder__(self):
@@ -108,33 +111,36 @@ class AutoEncoder:
         autoencoder.compile(optimizer='adam', loss='mse')
         return autoencoder
 
-    def train(self, epochs=100, gaps_datasets=[0,1], log_dir='./logs'):
+    def train(self, epochs=100, gaps_datasets=[0, 1], log_dir='./logs'):
         """
         this method will train the AutoEncoder.
         as default run 100 epochs in gaps dataset chunk 0 and 1. the training history will save in dir ```./logs```
         to see the history please using ```tensorboard```
         """
-        data = load_gaps_crack_images(gaps_datasets)
-        self.auto_encoder.fit(x=data[0:7000], y=data[0:7000],
+        # data = load_gaps_crack_images(gaps_datasets)
+        # data = load_gaps_cut('images/cut_image/', 5500)
+        data = np.load('images/cut_crack_image.npy')
+        self.auto_encoder.fit(x=data, y=data,
                               batch_size=16, epochs=epochs,
                               callbacks=[TensorBoard(log_dir='./logs')],
-                              validation_data=[data[7001:], data[7001:]])
+                              validation_split=0.1)
 
-    def test_model(self, model, weights_name):
-        model.load_weights(weights_name)
-        x, _ = load_gaps()
-        predict = model.predict(np.asarray([x[10]]))
-        plt.subplot(1, 2, 1)
-        plt.imshow(x[0, :, :, 0], cmap='gray')
-        plt.subplot(1, 2, 2)
-        plt.imshow(np.reshape(predict[0], (64, 64)), cmap='gray')
-        plt.show()
-        im = cv2.imread(
-            '/media/weibo/Data/Code/RoadDetect/test_img/timg.jpg', cv2.IMREAD_GRAYSCALE)
-        # im = cv2.imread('E:/Code/RoadDetect/test_img/th1.jpg', cv2.IMREAD_GRAYSCALE)
+    def test_model(self):
+        # x = load_gaps_crack_images([0])
+        # predict = self.auto_encoder.predict(np.asarray([x[10]]))
+        # plt.subplot(1, 2, 1)
+        # plt.imshow(x[0, :, :, 0], cmap='gray')
+        # plt.subplot(1, 2, 2)
+        # plt.imshow(np.reshape(predict[0], (64, 64)), cmap='gray')
+        # plt.show()
+        # im = cv2.imread(
+        #     '/media/weibo/Data/Code/RoadDetect/DeepLearning/images/images/train_0760.jpg', cv2.IMREAD_GRAYSCALE)
+        im = cv2.imread('../test_img/th1.jpg', cv2.IMREAD_GRAYSCALE)
+
         im = im.astype(np.float)
         im /= 255
-        ret = model.predict([im.reshape((1, im.shape[0], im.shape[1], 1))])
+        ret = self.auto_encoder.predict(
+            [im.reshape((1, im.shape[0], im.shape[1], 1))])
         plt.imshow(ret[0, :, :, 0], cmap='gray')
         plt.show()
 
@@ -142,4 +148,21 @@ class AutoEncoder:
         """
         return the encoder, decoder and autoencoder
         """
-        return  self.encoder, self.decoder, self.auto_encoder
+        return self.encoder, self.decoder, self.auto_encoder
+
+    def save_weights(self, weights_name):
+        self.encoder.save_weights(weights_name+"_encoder.h5")
+        self.decoder.save_weights(weights_name+"_decoder.h5")
+        self.auto_encoder.save_weights(weights_name+"_autoencoder.h5")
+
+
+def main(weights_name, epochs=100, pre_trained=False):
+    Auto_Encoder = AutoEncoder(
+        pre_trained=pre_trained, weights_path=weights_name)
+    Auto_Encoder.train(epochs=epochs)
+    Auto_Encoder.test_model()
+    Auto_Encoder.save_weights(weights_name)
+
+
+if __name__ == "__main__":
+    main('models/train_1122_1', 20, False)
---
+++ DeepLearning/voc_annotation.py
@@ -0,0 +1,35 @@
+import xml.etree.ElementTree as ET
+from os import getcwd
+
+# sets=[('2007', 'train'), ('2007', 'val'), ('2007', 'test')]
+
+# classes = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
+sets = [('2007', 'train')]
+classes = ['crack']
+
+
+def convert_annotation(year, image_id, list_file):
+    in_file = open('images/anno/%s.xml'%(image_id))
+    tree=ET.parse(in_file)
+    root = tree.getroot()
+
+    for obj in root.iter('object'):
+        difficult = obj.find('difficult').text
+        cls = obj.find('name').text
+        if cls not in classes or int(difficult)==1:
+            continue
+        cls_id = classes.index(cls)
+        xmlbox = obj.find('bndbox')
+        b = (int(xmlbox.find('xmin').text), int(xmlbox.find('ymin').text), int(xmlbox.find('xmax').text), int(xmlbox.find('ymax').text))
+        list_file.write(" " + ",".join([str(a) for a in b]) + ',' + str(cls_id))
+
+wd = getcwd()
+
+for year, image_set in sets:
+    image_ids = open('images/train_set/image_list.txt').read().strip().split()
+    list_file = open('%s_%s.txt'%(year, image_set), 'w')
+    for image_id in image_ids:
+        list_file.write('images/train_set/%s.jpg'%(image_id))
+        convert_annotation(year, image_id, list_file)
+        list_file.write('\n')
+    list_file.close()
